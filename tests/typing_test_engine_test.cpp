82d7a9c64639d513718d4f1e2bcbf3199a3a0b22
Anthony Leedom <anthonym.leedom@gmail.com>
2021-08-26 14:56:10 -0700

    Update tests to newer API

--- tests/typing_test_engine_test.cpp
+++ tests/typing_test_engine_test.cpp
@@ -4,42 +4,48 @@
 
 #include <typing_test_engine.hpp>
 
-TEST_CASE("Validation", "[Typing_test_engine]") {
+TEST_CASE("Validation", "[Typing_test_engine]")
+{
     using namespace std::chrono_literals;
 
     const int word_length{5};
     typer::Typing_test_engine engine;
 
-    engine.set_text("Test ");
-    CHECK(engine.commit_keystroke('T'));
-    CHECK(!engine.commit_keystroke('T'));
-    CHECK(engine.commit_keystroke('e'));
-    CHECK(engine.commit_keystroke('s'));
-    std::this_thread::sleep_for(2s);
-    CHECK(engine.commit_keystroke('t'));
-    CHECK(engine.commit_keystroke(' '));
-    CHECK(!engine.commit_keystroke('a'));
+    {
+        engine.set_text("Test ");
+        CHECK(engine.commit_keystroke('T').first);
+        CHECK(!engine.commit_keystroke('T').first);
+        CHECK(engine.commit_keystroke('e').first);
+        CHECK(engine.commit_keystroke('s').first);
+        std::this_thread::sleep_for(2s);
+        CHECK(engine.commit_keystroke('t').first);
+        CHECK(engine.commit_keystroke(' ').first);
+        auto const [b, stats] = engine.commit_keystroke('a');
+        CHECK(!b);
 
-    typer::Typing_test_engine::Stats stats{engine.get_stats()};
-    CHECK(stats.missed_keystrokes == 1);
-    CHECK(stats.correct_keystrokes == 5);
-    CHECK(stats.wpm == static_cast<int>((5.0 / word_length) / (2.0 / 60)));
+        CHECK(stats.missed_keystrokes == 1);
+        CHECK(stats.correct_keystrokes == 5);
+        CHECK(stats.wpm == static_cast<int>((5.0 / word_length) / (2.0 / 60)));
+    }
 
-    engine.set_text("Two");
-    CHECK(engine.commit_keystroke('T'));
-    CHECK(engine.commit_keystroke('w'));
-    std::this_thread::sleep_for(2s);
-    CHECK(engine.commit_keystroke('o'));
-    CHECK(!engine.commit_keystroke('o'));
+    {
+        engine.set_text("Two");
+        CHECK(engine.commit_keystroke('T').first);
+        CHECK(engine.commit_keystroke('w').first);
+        std::this_thread::sleep_for(2s);
+        CHECK(engine.commit_keystroke('o').first);
+        auto const [b, stats] = engine.commit_keystroke('o');
+        CHECK(!b);
 
-    stats = engine.get_stats();
-    CHECK(stats.missed_keystrokes == 0);
-    CHECK(stats.correct_keystrokes == 3);
-    CHECK(stats.wpm == static_cast<int>((3.0 / word_length) / (2.0 / 60)));
+        CHECK(stats.missed_keystrokes == 0);
+        CHECK(stats.correct_keystrokes == 3);
+        CHECK(stats.wpm == static_cast<int>((3.0 / word_length) / (2.0 / 60)));
+    }
 
-    engine.reset();
-    stats = engine.get_stats();
-    CHECK(stats.missed_keystrokes == 0);
-    CHECK(stats.correct_keystrokes == 0);
-    CHECK(stats.wpm == 0);
+    {
+        auto const stats = engine.reset();
+        CHECK(stats.missed_keystrokes == 0);
+        CHECK(stats.correct_keystrokes == 0);
+        CHECK(stats.wpm == 0);
+    }
 }
